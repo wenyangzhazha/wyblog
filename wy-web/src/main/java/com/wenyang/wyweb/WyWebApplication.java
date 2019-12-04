e308a558fd6dc259d5820320549016ae53a06f7b
wenyangjyyh <417440818@qq.com>
2019-12-04 17:52:54 +0800

    集成sagger2，存在日志打印问题 待解决

---
+++ wy-web/src/main/java/com/wenyang/wyweb/WyWebApplication.java
@@ -0,0 +1,17 @@
+package com.wenyang.wyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
+public class WyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WyWebApplication.class, args);
+    }
+
+}
---
+++ wy-web/src/main/java/com/wenyang/wyweb/config/Swagger2.java
@@ -0,0 +1,40 @@
+package com.wenyang.wyweb.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author wenyang
+ * @description TODO
+ * @since 2019/12/3 22:44
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("文阳搭建Swagger2")
+                .description("简单优雅的restfun风格，http://blog.csdn.net/saytime")
+                .termsOfServiceUrl("http://blog.csdn.net/saytime")
+                .version("1.0")
+                .build();
+    }
+}
