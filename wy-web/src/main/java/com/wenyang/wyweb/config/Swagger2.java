403dc9c4b43ff40c5a93d1d5780bdac2cf89739d
wenyangjyyh <417440818@qq.com>
2019-12-05 17:34:51 +0800

    集成可用swagger

--- wy-web/src/main/java/com/wenyang/wyweb/config/Swagger2.java
+++ wy-web/src/main/java/com/wenyang/wyweb/config/Swagger2.java
@@ -3,6 +3,8 @@ package com.wenyang.wyweb.config;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,24 +18,39 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @since 2019/12/3 22:44
  */
 @Configuration
-public class Swagger2 {
+public class Swagger2 extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                // 扫描的包路径
+                .apis(RequestHandlerSelectors.basePackage("com.wenyang.wyweb.restapi"))
+                // 定义要生成文档的Api的url路径规则
                 .paths(PathSelectors.any())
+                .build()
+                // 设置swagger-ui.html页面上的一些元素信息。
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                // 标题
+                .title("SpringBoot集成Swagger2")
+                // 描述
+                .description("这是一篇博客演示")
+                // 文档版本
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("文阳搭建Swagger2")
-                .description("简单优雅的restfun风格，http://blog.csdn.net/saytime")
-                .termsOfServiceUrl("http://blog.csdn.net/saytime")
-                .version("1.0")
-                .build();
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
--- wy-web/src/main/java/com/wenyang/wyweb/restapi/LoginController.java
+++ wy-web/src/main/java/com/wenyang/wyweb/restapi/LoginController.java
@@ -1,6 +1,8 @@
 package com.wenyang.wyweb.restapi;
 
 import com.wenyang.wyblog.entity.Admin;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/admin")
+@Api(description = "管理员相关接口")
 public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ApiOperation("管理员测试")
     public Admin login(Admin admin){
         return admin;
     }
